d9aef821311e16a855b19cbe21dc59812709d3b7
ub-victor <victoireushindi371@gmail.com>
2026-03-26 10:23:01 +0200

    Initial commit

--- Basic/welcome.php
+++ Basic/welcome.php
@@ -5,6 +5,23 @@
     <title>Welcome</title>
 </head>
 <body>
-    
+    <p >Welcome dear</p>
+    <p>Your name is: <?php echo $_POST["name"]; ?></p>
+    <br>
+    <p>Your email is: <?php echo $_POST["email"]; ?></p>
+    <br>
+    <button onclick="window.location.href='phpForm.php'">Go Back</button>
 </body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Ushindi123!";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 </html>
