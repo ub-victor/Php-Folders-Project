68bdf960b806fe29a5771d80590ea4240f49a8d8
ub-victor <victoireushindi371@gmail.com>
2026-03-26 09:36:24 +0200

    var

--- mywebsite/index.php
+++ mywebsite/index.php
@@ -6,18 +6,20 @@
 </head>
 <body>
     <?php 
-        $name = "Ushindi Bihame VIcotir";
+        $name = "Ushindi Bihame Victoire";
     ?>
      <p>Hi! My name is <?php echo $name?>, and I'm learning PHP! </p>
 
     <?php  
-        echo "Hello World"
+        echo "Hello World";
     ?>
 
+    <?php
     $n1 = 30;
     $n2 = 60;
-    $sum = $n1+ $n2;
-    echo ($sum)
-    echo strlen("How many charactors in this")
+    $sum = $n1+$n2;
+    echo ($sum);
+    echo strlen("How many charactors in this");
+    ?>
 </body>
 </html>
