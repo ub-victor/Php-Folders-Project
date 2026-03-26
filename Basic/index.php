829a77d9922f049ab7ed210a80618c838a9ef597
ub-victor <victoireushindi371@gmail.com>
2026-03-26 09:40:43 +0200

    var

---
+++ Basic/index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $name = "Ushindi Bihame Victoire";
+    ?>
+     <p>Hi! My name is <?php echo $name?>, and I'm learning PHP! </p>
+
+    <?php  
+        echo "Hello World";
+    ?>
+
+    <?php
+    $n1 = 30;
+    $n2 = 60;
+    $sum = $n1+$n2;
+    echo ($sum);
+    echo strlen("How many charactors in this");
+    ?>
+</body>
+</html>
