501ed7726a3b5b5aabe15ee5580f93dd25e83d5f
ub-victor <victoireushindi371@gmail.com>
2026-03-26 09:48:42 +0200

    if else

---
+++ Basic/ifelse.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $d=date("D");
+if ($d=="Fri"){
+echo "Have a nice weekend!";
+}else{
+echo "Have a nice day!";
+}
+    
+    ?>
+</body>
+</html>
