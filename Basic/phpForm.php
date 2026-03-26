1a74d00011eb8c94de8bac46ab694a5044b813a7
ub-victor <victoireushindi371@gmail.com>
2026-03-26 09:55:15 +0200

    Initial commit

--- Basic/phpForm.php
+++ Basic/phpForm.php
@@ -5,6 +5,16 @@
     <title>PHP Forms and User Input</title>
 </head>
 <body>
+    <pre>
+        <form action="welcome.php" method="post">
+    Name: <input type="text" name="name">
+    <br>
+    E-mail: <input type="text" name="email">
+    <br>
+    <input type="submit">
+
+    </pre>
     
+</form>
 </body>
 </html>
