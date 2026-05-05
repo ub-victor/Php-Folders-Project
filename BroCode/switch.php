958688c65868ac7197fa7e5bd9f79f81daf012b3
ub-victor <victoireushindi371@gmail.com>
2026-05-05 18:13:05 +0200

    basics

--- BroCode/switch.php
+++ BroCode/switch.php
@@ -1,21 +1,22 @@
+
 $grade = "A";
 
 switch($grade){
-	case "A":
-		echo "You did well";
-		break;
-	case "B":
-		echo "You did good";
-		break;
-	case "C":
-		echo "You did okay";
-		break;
-	case "D":
-		echo "You did poorly";
-		break;
-	case "F":
-		echo "You failed";
-		break;
-	default:
-		echo "{$grade} is not valid";
+    case "A":
+        echo "You did well";
+        break;
+    case "B":
+        echo "You did good";
+        break;
+    case "C":
+        echo "You did okay";
+        break;
+    case "D":
+        echo "You did poorly";
+        break;
+    case "F":
+        echo "You failed";
+        break;
+    default:
+        echo $grade . " is not valid";
 }
