0074c9c55fc8e3f1082342321d706aa0cb6cc8ac
ub-victor <victoireushindi371@gmail.com>
2026-05-05 14:56:41 +0200

    basics

--- BroCode/resto.php
+++ BroCode/resto.php
@@ -7,7 +7,10 @@
 </head>
 <body>
     <form action="resto.php" method="post">
-        
+        <h6>How many Pizza you want , one cost: <?php echo  ?> </h6>
+        <label>Quantity: </label><br>
+        <input type="text" name="quantity">
+        <input type="submit" value="total">
     </form>
 </body>
 </html>
