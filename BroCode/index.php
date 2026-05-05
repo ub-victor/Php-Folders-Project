aaa16db7c73e0df629661c7103a9eb4478242286
ub-victor <victoireushindi371@gmail.com>
2026-05-05 14:14:54 +0200

    basics

--- BroCode/index.php
+++ BroCode/index.php
@@ -16,7 +16,6 @@
         <label for="password">Password: </label><br>
         <input type="password" name="password"><br>
         <input type="submit" value="Log in">
-
     </form>
 </body>
 </html>
