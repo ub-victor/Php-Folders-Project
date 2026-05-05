b6fca0efd77e2f46142e1eb9b9724cb7baae2073
ub-victor <victoireushindi371@gmail.com>
2026-05-05 10:29:24 +0200

    basics

--- BroCode/index.php
+++ BroCode/index.php
@@ -2,3 +2,21 @@
     echo "Hello World!";
     echo '<button>Submit!</button>';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order Pizza</button>
+    <br>
+</body>
+</html>
+<!-- Variables -->
+
+<?php
+    $name = "Victoire Ushindi"
+?>
