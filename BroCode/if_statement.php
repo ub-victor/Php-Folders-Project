2edda624c30cec656035fadd14c96bf89c0aaf6e
ub-victor <victoireushindi371@gmail.com>
2026-05-05 18:09:25 +0200

    basics

---
+++ BroCode/if_statement.php
@@ -0,0 +1,11 @@
+<?php
+		$age = 21;
+	
+		if($age >= 18){
+			echo "You may enter this site";
+		}elseif($age == 0){
+			echo "You were just born";
+		}else{
+			echo "You must be 18+ to enter";
+		}
+ ?>
