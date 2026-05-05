e8b84625d0eab1b8ff7511fbd01c30574209397a
ub-victor <victoireushindi371@gmail.com>
2026-05-05 23:41:03 +0200

    basics

--- BroCode/index.php
+++ BroCode/index.php
@@ -51,7 +51,7 @@
     // add an item in the array we use the array_push
     // to remove the first element we use array_shift and the last one we use pop
     // array_pop($food);
-    array_shift($food);
+    array_shift($foods);
     array_push($foods, "pineapple");
     echo "<br> Let us use the foreach  <br>";
 
