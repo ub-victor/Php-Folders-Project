99de8ebeff6ff5d2c3f952ced5254e6676da6a57
ub-victor <victoireushindi371@gmail.com>
2026-05-05 18:21:26 +0200

    basics

---
+++ BroCode/days.php
@@ -0,0 +1,29 @@
+<?php
+$day = date("l"); //returns the full day name 
+
+switch($day) {
+    case "Monday":
+        echo "Start of the work week";
+        break;
+    case "Tuesday":
+        echo "Second day";
+        break;
+    case "Wednesday":
+        echo "Midweek";
+        break;
+    case "Thursday":
+        echo "Almost Friday";
+        break;
+    case "Friday":
+        echo "TGIF!";
+        break;
+    case "Saturday":
+        echo "Weekend!";
+        break;
+    case "Sunday":
+        echo "Rest day";
+        break;
+    default:
+        echo $day . " is not valid";
+}
+?>
