7075d7363b4ee5ceae7c25dacc22d8e6a96a7427
ub-victor <victoireushindi371@gmail.com>
2026-05-06 20:20:38 +0200

    basics

--- BroCode/session/home.php
+++ BroCode/session/home.php
@@ -2,6 +2,19 @@
     session_start()
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the Home Page <br>
+    <a href="home.php">This goes to the Index Page</a> <br>
+</body>
+</html>
+
 <?php
     echo $_SESSION["username"] . "<br>";
     echo $_SESSION["password"] . "<br>";
