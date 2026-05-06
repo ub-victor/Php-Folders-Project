7e483ca694ad91a80f7eb0f1275dede02fcdc01a
ub-victor <victoireushindi371@gmail.com>
2026-05-07 01:21:03 +0200

    basics

--- BroCode/mysql/index.php
+++ BroCode/mysql/index.php
@@ -2,7 +2,8 @@
 // I included this file so  $conn becomes available in this file.
 include("database.php"); 
 
-
+    $username = "";
+    $password = "";
 
 $sql = "INSERT INTO users (user, password)
         VALUES ('Ushindi', 'Ushindi@123!')";
