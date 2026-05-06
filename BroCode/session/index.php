541785bcbb600b1d1a025cc82395d8b3e182c546
ub-victor <victoireushindi371@gmail.com>
2026-05-06 20:24:51 +0200

    basics

--- BroCode/session/index.php
+++ BroCode/session/index.php
@@ -15,7 +15,7 @@
         Username: <br>
         <input type="text" name="usename"> <br>
         Username: <br>
-        <input type="text" name="usename"> <br>
+        <input type="text" name="password"> <br>
     </form>
 </body>
 </html>
