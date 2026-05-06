f30e373b3174c0a7092cab9bf7f258f84aae5d36
ub-victor <victoireushindi371@gmail.com>
2026-05-06 20:23:50 +0200

    basics

--- BroCode/session/index.php
+++ BroCode/session/index.php
@@ -10,9 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    This is the login Page <br>
-    <a href="home.php">This goes to the Home Page</a> <br>
+<body> 
+    <form action="index.php"></form>
 </body>
 </html>
 
