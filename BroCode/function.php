835acc9847efc615f0097f4d30a9245a8c047cc0
ub-victor <victoireushindi371@gmail.com>
2026-05-06 16:10:08 +0200

    basics

--- BroCode/function.php
+++ BroCode/function.php
@@ -24,7 +24,7 @@
     function is_even($number){
         return $number % 2 == 0
             ? "The number is even"
-            : "The number isn't even"
+            : "The number isn't even";
     }
 
     echo is_even(10);
