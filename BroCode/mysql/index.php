955893e0acfe9502b24c5156cb8d976cfa0a88e7
ub-victor <victoireushindi371@gmail.com>
2026-05-07 00:20:36 +0200

    basics

--- BroCode/mysql/index.php
+++ BroCode/mysql/index.php
@@ -2,7 +2,7 @@
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "Ushindi123!";
-    $db_user = "";
+    $db_name = "Brocode";
     $conn = "";
 
     $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name)
