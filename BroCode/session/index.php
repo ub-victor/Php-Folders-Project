9f20a295f41a1dbf97b5e8192df42c7637b5542c
ub-victor <victoireushindi371@gmail.com>
2026-05-06 20:30:45 +0200

    basics

--- BroCode/session/index.php
+++ BroCode/session/index.php
@@ -25,10 +25,12 @@
 <?php 
 
     if(isset($_POST["login"])){
-        
+
+        $_SESSION["username"] = $_POST["username"] ;
+        $_SESSION["password"] = $_POST["password"];
+
     }
-    $_SESSION["username"] = $_POST["username"] ;
-    $_SESSION["password"] = $_POST["password"];
+    
 
 
 
