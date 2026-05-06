d318e4db950f47dc2cca79c77027bf9f1f74a23c
ub-victor <victoireushindi371@gmail.com>
2026-05-06 21:53:20 +0200

    basics

--- BroCode/server/start.php
+++ BroCode/server/start.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="<?php $_SERVER["PHP_SELF"] ?>" method="post">
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
         Username <br>
         <input type="text" name="username"> <br>
         <input type="submit" name="submit"> <br>
