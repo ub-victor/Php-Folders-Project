80b13bdb3e6100dbb2c2f57c0659a6195af00b96
ub-victor <victoireushindi371@gmail.com>
2026-05-07 00:36:23 +0200

    basics

--- BroCode/mysql/database.php
+++ BroCode/mysql/database.php
@@ -10,6 +10,9 @@ $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
     if($conn){
         echo "Database connected successfully!";
     }else{
+        // If database connection fails: returns the error message.
+        // Then die() , Prints the message
+Stops the script completely
         die("Connection failed: " . mysqli_connect_error());
     }
 
