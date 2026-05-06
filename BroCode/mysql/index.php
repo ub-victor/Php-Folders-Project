7ea283021a59fb633703bfa5a1c4bbddccde2131
ub-victor <victoireushindi371@gmail.com>
2026-05-07 01:32:14 +0200

    basics

--- BroCode/mysql/index.php
+++ BroCode/mysql/index.php
@@ -1,30 +1,12 @@
 <?php
-// I included this file so  $conn becomes available in this file.
-include("database.php"); 
 
-    $username = "";
-    $password = "";
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-
-$sql = "INSERT INTO users (user, password)
-        VALUES ('$username', '$hash')";
-
-try{
-    //Sends the SQL command to MySQL. which database connection and SQL command to execute
-    mysqli_query($conn, $sql);
-    echo "<br>";
-    echo "<br>User is now registered";
-
-}catch(mysqli_sql_exception){
-    echo "Could not register user";
-}
-// Closes database connection.
-mysqli_close($conn);
 
 ?>
 
 <!-- to retrieve form the db -->
 <?php 
+    include("database.php");
+
     $sql = "SELECT * FROM users WHERE user = 'Ushindi'";
     $result = mysqli_query($conn, $sql);
 
