4a1bd65fdef0f33d59a74feb1cd97b8a3e1357c4
ub-victor <victoireushindi371@gmail.com>
2026-05-07 00:23:20 +0200

    basics

---
+++ BroCode/mysql/database.php
@@ -0,0 +1,16 @@
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "Ushindi123!";
+    $db_name = "Brocode";
+    $conn = "";
+
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+
+    if($conn){
+        echo "The Db is running";
+    }else{
+        echo "The Db isn't running";
+    }
+
+?>
