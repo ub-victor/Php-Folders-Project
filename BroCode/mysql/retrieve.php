862088f9b48f938b12708d72515fbbf7c357cd03
ub-victor <victoireushindi371@gmail.com>
2026-05-07 01:53:47 +0200

    basics

--- BroCode/mysql/retrieve.php
+++ BroCode/mysql/retrieve.php
@@ -5,6 +5,7 @@
     $sql = "SELECT * FROM users WHERE user = 'Ushindis'";
     $result = mysqli_query($conn, $sql);
 
+    // Checks if query returned any rows
     if(mysqli_num_rows($result)> 0){
         $row = mysqli_fetch_assoc($result); // if it could be to retrieve all user , we could pull the line of code in a while loop
         echo $row["id"] . "<br>";
