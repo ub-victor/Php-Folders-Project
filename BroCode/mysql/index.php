eca43da8135a51423c13ceca2e0b3d4707832ccb
ub-victor <victoireushindi371@gmail.com>
2026-05-07 01:14:51 +0200

    basics

--- BroCode/mysql/index.php
+++ BroCode/mysql/index.php
@@ -7,7 +7,7 @@ $sql = "INSERT INTO users (user, password)
 
 //Sends the SQL command to MySQL. which database connection and SQL command to execute
 mysqli_query($conn, $sql);
-
+// Closes database connection.
 mysqli_close($conn);
 
 ?>
