02184f58ebe3cd8f0e95e4f439821eadb6467687
ub-victor <victoireushindi371@gmail.com>
2026-05-07 01:35:38 +0200

    basics

--- BroCode/mysql/retrieve.php
+++ BroCode/mysql/retrieve.php
@@ -1,10 +1,16 @@
 <?php 
     include ( "database.php");
-    
+
     $sql = "SELECT * FROM users WHERE user = 'Ushindi'";
     $result = mysqli_query($conn, $sql);
 
+    
+    
+    
+    
     mysqli_close($conn)
+
+
 
 
 ?>
