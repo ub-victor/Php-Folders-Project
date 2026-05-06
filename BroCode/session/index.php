cb527f89bf26714ed4ddad5e19978182f0001f39
ub-victor <victoireushindi371@gmail.com>
2026-05-06 20:35:27 +0200

    basics

--- BroCode/session/index.php
+++ BroCode/session/index.php
@@ -31,8 +31,8 @@
 
         if(!empty($_POST["username"]) &&
             !empty($_POST["password"])){
-                echo $_POST["username"];
-                echo $_POST["password"];
+                $_SESSION["username"] = $_POST["username"];
+                $_SESSION["password"] = $_POST["password"];
             }
     }
 ?>
