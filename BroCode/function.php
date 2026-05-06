18d119dc68740b73806479f1b81a15523260be8e
ub-victor <victoireushindi371@gmail.com>
2026-05-06 15:48:59 +0200

    basics

--- BroCode/function.php
+++ BroCode/function.php
@@ -1,5 +1,5 @@
 <?php 
-    function birthDay($name, $age){
+    function birthday($name, $age){
         echo "Happy Birthday to you <br>";
         echo "Happy Birthday dear {$name} <br>";
         echo "you are  {$age} years old <br>";
