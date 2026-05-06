1d332b24b8d4e8e011412c1a300621cc11b94668
ub-victor <victoireushindi371@gmail.com>
2026-05-07 01:18:09 +0200

    basics

--- BroCode/mysql/index.php
+++ BroCode/mysql/index.php
@@ -8,7 +8,8 @@ $sql = "INSERT INTO users (user, password)
 try{
     //Sends the SQL command to MySQL. which database connection and SQL command to execute
     mysqli_query($conn, $sql);
-    echo "User is now registered";
+    echo "<br>";
+    echo "<br>User is now registered";
 
 }catch(mysqli_sql_exception){
     echo "Could not register user";
