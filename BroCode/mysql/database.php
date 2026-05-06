3111cd820609a9cb1c29f8d4aa61bbe3621e477e
ub-victor <victoireushindi371@gmail.com>
2026-05-07 00:39:51 +0200

    basics

--- BroCode/mysql/database.php
+++ BroCode/mysql/database.php
@@ -7,8 +7,8 @@ $db_name = "Brocode";
 
 try{
     $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-}catch(){
-
+}catch(mysqli_sql_exception){
+    echo "Could not connect!"
 }
 
 ?>
