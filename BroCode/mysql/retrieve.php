8c681617bd8a05461cac8979f6cc1fb0c5c483c4
ub-victor <victoireushindi371@gmail.com>
2026-05-07 01:51:31 +0200

    basics

--- BroCode/mysql/retrieve.php
+++ BroCode/mysql/retrieve.php
@@ -5,7 +5,7 @@
     $result = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($result)> 0){
-        $row = mysqli_fetch_assoc($result);
+        $row = mysqli_fetch_assoc($result); // if it could be to retrieve all user , we could pull the line of code in a while loop
         echo $row["id"] . "<br>";
         echo $row["user"] . "<br>";
         echo $row["reg_date"] . "<br>";
