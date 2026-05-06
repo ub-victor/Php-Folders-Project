3e980528b23c353e803aefcbf88fe8adfe658025
ub-victor <victoireushindi371@gmail.com>
2026-05-07 01:02:52 +0200

    basics

--- BroCode/mysql/index.php
+++ BroCode/mysql/index.php
@@ -5,6 +5,7 @@ include("database.php");
 $sql = "INSERT INTO users (user, password)
         VALUES ('Ushindi', 'Ushindi@123!')";
 
+//Sends the SQL command to MySQL.
 mysqli_query($conn, $sql);
 
 mysqli_close($conn);
