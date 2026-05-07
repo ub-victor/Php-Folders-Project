053f9c56a426ffe350c8c0d9b2f4da867020e36f
ub-victor <victoireushindi371@gmail.com>
2026-05-07 02:13:02 +0200

    basics

---
+++ BroCode/registration_form/database.php
@@ -0,0 +1,18 @@
+<?php
+
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "Ushindi123!";
+$db_name = "Brocode";
+$conn = "";
+
+try{
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+}catch(mysqli_sql_exception){
+    echo "Could not connect!";
+}
+if($conn){
+    echo "Database connected successfully! <br>";
+    echo "<br>";
+}
+?>
