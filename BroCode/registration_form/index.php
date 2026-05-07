72a4fa978e3abb7f357fdf3645c3f28dfcc6158d
ub-victor <victoireushindi371@gmail.com>
2026-05-07 02:15:49 +0200

    basics

--- BroCode/registration_form/index.php
+++ BroCode/registration_form/index.php
@@ -2,8 +2,17 @@
     include("database.php")
 
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <form action="<?php ?>" method="post"></form>
+</body>
+</html>
 <?php 
-
 mysqli_close($conn)
 ?>
