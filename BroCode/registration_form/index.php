fb95666cf67d636487affaf1f1bb3579bed038eb
ub-victor <victoireushindi371@gmail.com>
2026-05-07 02:24:54 +0200

    basics

--- BroCode/registration_form/index.php
+++ BroCode/registration_form/index.php
@@ -24,7 +24,7 @@
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 }
 
 mysqli_close($conn)
