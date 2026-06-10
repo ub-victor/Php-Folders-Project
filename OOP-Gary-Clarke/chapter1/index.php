218d9d312b916e5f261d01d723e298ed173d0274
ub-victor <victoireushindi371@gmail.com>
2026-06-10 15:41:50 +0200

    classes

--- OOP-Gary-Clarke/chapter1/index.php
+++ OOP-Gary-Clarke/chapter1/index.php
@@ -10,7 +10,7 @@ $anotherProduct = new Product();
 
 // var_dump() is a built-in PHP function used for debugging.
 //It prints detailed information about a variable, including:its type, its structure, its values
-var_dump($product); // it will output the object
+//var_dump($product); // it will output the object
 $soapProduct = new Product();
 // → object operator (used to access properties or methods)
 // print → outputs the value to the screen
