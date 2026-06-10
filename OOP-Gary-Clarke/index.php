da2aa338702a98096c8b1c09b76f678dfc5cd6a7
ub-victor <victoireushindi371@gmail.com>
2026-06-10 14:56:44 +0200

    intro to class

--- OOP-Gary-Clarke/index.php
+++ OOP-Gary-Clarke/index.php
@@ -7,6 +7,7 @@ class Product{
 $product = new Product();
 
 // var_dump() is a built-in PHP function used for debugging.
+//It prints detailed information about a variable, including:its type, its structure, its values
 var_dump($product);
 
 ?>
