01448c6c4925ca32343a950bca58e738522d812a
ub-victor <victoireushindi371@gmail.com>
2026-06-14 22:27:18 +0200

    method

--- OOP-Gary-Clarke/chapter1/class-methods-arg.php
+++ OOP-Gary-Clarke/chapter1/class-methods-arg.php
@@ -1,5 +1,13 @@
 <?php
+    class Product {
+        public $name;
+        public $price;
 
+        public function priceAsCurrency(){
+            $priceAsCurrency = $this->price / 100;
+            return $priceAsCurrency;
+        }
+    }
 
 
 ?>
